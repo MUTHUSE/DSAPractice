3fbdb44ef8b1be898f861d8072aaa48eb3d4afa1
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-20 09:05:24 +0530

    Create 5.multipleInput.cpp

---
+++ 1. Basics/5.multipleInput.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"Enter the a and b values : ";
+    cin>>a>>b;
+    cout<<"a value : "<<a<<" b value : "<<b;
+
+    return 0;
+}
