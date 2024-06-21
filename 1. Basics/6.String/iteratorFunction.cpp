666aa29bf87ae80a26d9d040dd1812bea6940c71
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-21 09:28:56 +0530

    Create iteratorFunction.cpp

---
+++ 1. Basics/6.String/iteratorFunction.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name("Muthu selvi");
+    cout<<"\n String Data : ";
+    for(auto it = name.begin(); it != name.end(); it++){
+        cout<<*it<<" ";
+    }
+    return 0;
+}
