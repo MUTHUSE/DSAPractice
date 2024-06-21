657cade95b996e0194607110880fab3bde68ec27
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-21 09:24:36 +0530

    Create 3.capacityFunction.cpp

---
+++ 1. Basics/6.String/3.capacityFunction.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str("Muthu selvi");
+    cout<<"\n Size of the String : "<<str.size();
+    cout<<"\n Length of the String : "<<str.length();
+    cout<<"\n Max Size of the String : "<<str.max_size();
+    return 0;
+}
