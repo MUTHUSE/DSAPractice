54db7ea79851f80c5c2ae093a516202674fe8f3a
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-22 09:37:22 +0530

    Create 1.ifStatement.cpp

---
+++ 1. Basics/7.conditionalStatement/1.ifStatement.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+// all the if statement is to be check when use multiple if clause
+int main()
+{
+    int a, b;
+    cout<<"\n Enter the a & b value are : ";
+    cin>>a>>b;
+    
+    if(a>b){
+        cout<<"\n a is greater than b.";
+    }
+    if(a<b){
+        cout<<"\n b is greater than a.";
+    }
+    if(a==b){
+        cout<<"\n a & b are equal.";
+    }
+    return 0;
+}
