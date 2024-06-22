d95034f4e6315a92f2204c7e679bc88a62bdc729
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-22 09:31:00 +0530

    Update 4.iteratorFunction.cpp

--- 1. Basics/6.String/4.iteratorFunction.cpp
+++ 1. Basics/6.String/4.iteratorFunction.cpp
@@ -8,5 +8,20 @@ int main()
     for(auto it = name.begin(); it != name.end(); it++){
         cout<<*it<<" ";
     }
+    
+    cout<<"\n Iteration Method : ";
+    for(string::iterator it = name.begin(); it != name.end(); it++){
+        cout<<*it<<" ";
+    }
+    
+    cout<<"\n Another Method : ";
+    for(auto it:name){
+        cout<<it<<" ";
+    }
+    
+    cout<<"\n Reverse Iteration : ";
+    for(string::reverse_iterator it = name.rbegin(); it != name.rend(); it++){
+        cout<<*it<<" ";
+    }
     return 0;
 }
