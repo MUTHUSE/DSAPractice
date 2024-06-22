852977038a259c5d87cb34c9b2daa82947fced76
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-22 09:33:20 +0530

    Create 5.stringManipulatingFunction.cpp

---
+++ 1. Basics/6.String/5.stringManipulatingFunction.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name("Muthu selvi");
+    
+    string name1("Siva Lingam");
+    
+    name.swap(name1);
+    
+    cout<<"\n String name value is : "<<name;
+    cout<<"\n String name1 value is : "<<name1;
+    return 0;
+}
