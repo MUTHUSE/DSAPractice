d441e01300ebd51f30354f70c5b1bd1ab37d0eab
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-25 09:23:41 +0530

    Create 2.doWhile.cpp

---
+++ 1. Basics/8.LoopingStatement/2.doWhile.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int limit;
+    cout<<"\n Enter the limit : ";
+    cin>>limit;
+    int i = 1;
+    do{
+        cout<<i<<"\n";
+        i++;
+    }while(i<=limit);
+    return 0;
+}
