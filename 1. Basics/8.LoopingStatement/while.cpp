211fe157f510cb9b63a1d2e950e79178aab24dd1
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-25 09:22:11 +0530

    Create while.cpp

---
+++ 1. Basics/8.LoopingStatement/while.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int limit;
+    cout<<"\n Enter the limit : ";
+    cin>>limit;
+    int i = 1;
+    while(i<=limit){
+        cout<<i<<"\n";
+        i++;
+    }
+    return 0;
+}
