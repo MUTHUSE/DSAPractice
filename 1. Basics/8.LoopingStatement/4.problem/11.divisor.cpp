0adb1c8259db384541e9424bb924ea56917e7737
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-25 14:56:03 +0530

    Create 11.divisor.cpp

---
+++ 1. Basics/8.LoopingStatement/4.problem/11.divisor.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"\n Enter the number : ";
+    cin>>n;
+    
+    cout<<"\n All the divisor of n is : ";
+    for(int i = 1; i*i<=n; i++){
+        if(n%i == 0){
+            cout<<i<<" ";
+            if((n%(n/i)==0) && ((n/i)!=i)){
+                cout<<(n/i)<<" ";
+            }
+        }
+    }
+
+    return 0;
+}
