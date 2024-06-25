8ce6498f1429d72ac371fd5b933d3e6ee285b186
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-25 09:24:41 +0530

    Create 3.forLoop.cpp

---
+++ 1. Basics/8.LoopingStatement/3.forLoop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int limit;
+    cout<<"\n Enter the limit : ";
+    cin>>limit;
+    for(int i = 1; i<=limit;i++){
+        cout<<i<<"\n";
+    }
+    return 0;
+}
