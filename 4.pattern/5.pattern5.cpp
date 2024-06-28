2fd5cdd0aed819769f03aa552b1864af6ccfe1a9
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-28 09:12:43 +0530

    Create 5.pattern5.cpp

---
+++ 4.pattern/5.pattern5.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ *       *****
+ *       ****
+ *       ***
+ *       **
+ *       *
+*/
+
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<"\n Enter the n value : ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
