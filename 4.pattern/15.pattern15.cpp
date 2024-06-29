3a4d27262cb57da190f82ab0dae4dcc6f598295e
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-06-29 08:54:26 +0530

    Create 15.pattern15.cpp

---
+++ 4.pattern/15.pattern15.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+/**
+ *          ABCDE
+ *          ABCD
+ *          ABC
+ *          AB
+ *          A
+*/
+int main() {
+    // Write C++ code here
+    int n;
+    cout<<"\n Enter the n value : ";
+    cin>>n;
+    
+    for(int i=n;i>0;i--){
+        char ch = 'A';
+        for(int j=0;j<i;j++){
+            char c = ch + j;
+            cout<<c;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
