5744dc1ecac048ced14ddc934723256189a86148
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-07-11 14:47:15 +0530

    Create 25.pattern25.cpp

---
+++ 4.pattern/25.pattern25.cpp
@@ -0,0 +1,28 @@
+/**
+Enter the n value : 5
+----*
+---***
+--*****
+-*******
+*********
+  */
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"\n Enter the n value : ";
+    cin>>n;
+    for(int i = 0;i<n;i++){
+        for(int j = i;j<n-1;j++){
+            cout<<"-";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
