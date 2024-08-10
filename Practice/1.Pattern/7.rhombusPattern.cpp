1aff47c9a5d30b204fe3442690e059500d301955
MUTHU SELVI <56651900+MUTHUSE@users.noreply.github.com>
2024-08-10 14:15:03 +0530

    Update 7.rhombusPattern.cpp

--- Practice/1.Pattern/7.rhombusPattern.cpp
+++ Practice/1.Pattern/7.rhombusPattern.cpp
@@ -1,9 +1,30 @@
 // Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
+/*
+    Enter the n value : 5
+
+ Rhombus Pattern 
+     * * * * * 
+    * * * * * 
+   * * * * * 
+  * * * * * 
+ * * * * * 
+     1 2 3 4 5 
+    1 2 3 4 5 
+   1 2 3 4 5 
+  1 2 3 4 5 
+ 1 2 3 4 5 
+     A B C D E 
+    A B C D E 
+   A B C D E 
+  A B C D E 
+ A B C D E 
+    */
 class base{
     private:
-        int i,j;
+        int i,j,count;
+        char ch;
     public:
         void pattern(int n){
             for(i=0;i<n;i++){
@@ -16,11 +37,31 @@ class base{
                 cout<<"\n";
             }
         }
-};
-class derived:public base{
-    public:
-        void pattern(){
-            cout<<"Derived Class";
+        void patternNum(int n){
+            for(i=0;i<n;i++){
+                for(j=i;j<n;j++){
+                    cout<<" ";
+                }
+                count = 1;
+                for(j=0;j<n;j++){
+                    cout<<count<<" ";
+                    count+=1;
+                }
+                cout<<"\n";
+            }
+        }
+        void patternChar(int n){
+            for(i=0;i<n;i++){
+                for(j=i;j<n;j++){
+                    cout<<" ";
+                }
+                ch = 'A';
+                for(j=0;j<n;j++){
+                    cout<<ch<<" ";
+                    ch+=1;
+                }
+                cout<<"\n";
+            }
         }
 };
 int main() {
@@ -29,9 +70,9 @@ int main() {
     cin>>n;
     cout<<"\n Rhombus Pattern \n";
     
-    derived d;
-    base *b = NULL;
-    b = &d;
-    b->pattern(n);
+    base b;
+    b.pattern(n);
+    b.patternNum(n);
+    b.patternChar(n);
     return 0;
 }
